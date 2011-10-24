bb1bd6345ca70ca346ebfe4f054eb8db4e6b1770
AllierG@gmail.com <AllierG@gmail.com@af060f70-a1fd-ab6b-c1e5-f9016cbe30fc>
2011-10-24 19:42:47 +0000

    TODO:
    1. Add keyboard entry
    2. Add "real-time" conversation

---
+++ trunk/ software-quality-and-testing-lab-project/BinDecConverter/BinDecConverter.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinDecConverter
+{
+    public static class BinDecConverter
+    {
+        internal static string DecToBin(string Decimal)
+        {
+            int index = Decimal.IndexOf(',');
+            if (index < 0)
+            {
+                Decimal += ',';
+                index = Decimal.Length - 1;
+            }
+            Int64 intPart = Int64.Parse(Decimal.Substring(0, index));
+            
+            Double decPart = 0;
+            bool hasDec = (Decimal.Length - index > 1 ? Double.TryParse(Decimal.Substring(index, Decimal.Length - index), out decPart) : false);
+            if (decPart == 0)
+                hasDec = false;
+
+            string result = (intPart % 2).ToString();
+            intPart >>= 1;
+            while (intPart > 0)
+            {
+                result = (intPart % 2).ToString() + result;
+                intPart >>= 1;
+            }
+
+            if (!hasDec)
+                return result;
+
+            StringBuilder dec_string = new StringBuilder();
+            while (decPart > 0)
+            {
+                if ((result.Length + dec_string.Length) > 64)
+                    break;
+                if (decPart == 1)
+                {
+                    dec_string.Append(decPart);
+                    break;
+                }
+                decPart = decPart * 2;
+                if (decPart >= 1)
+                {
+                    dec_string.Append(1);
+                    decPart -= 1;
+                }
+                else
+                    dec_string.Append(0);
+            }
+
+            return (result + "," + dec_string);
+        }
+        
+        internal static string BinToDec(string Binary)
+        {
+            int length = Binary.Length;
+            int index = Binary.IndexOf(',');
+            if (index < 0)
+            {
+                Binary += ',';
+                index = length - 1;
+            }
+            Int64 intPart = Convert.ToInt64(Binary.Substring(0, index), 2);
+            string result = intPart.ToString();
+
+            if (length - index < 1)
+                return result;
+
+            int decPartLength = length - index - 1;
+            string decPart = Binary.Substring(index + 1);
+
+            double decResult = 0;
+            for (int i = 1; i <= decPartLength; i++)
+            {
+                if (decPart[i - 1] == '1')
+                    decResult += Math.Pow(2, -i);
+            }
+
+            return result + "," + decResult.ToString().Substring(2);
+        }
+    }
+}
---
+++ trunk/ software-quality-and-testing-lab-project/BinDecConverter/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+
+namespace BinDecConverter
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void tbOut_TextChanged(object sender, EventArgs e)
+        {
+            tbOut.SelectionStart = tbOut.Text.Length;
+            tbOut.ScrollToCaret();
+            if (tbOut.Text.Length > 0)
+                gbSystems.Enabled = false;
+            else
+                gbSystems.Enabled = true;
+        }
+
+        private void rbBinary_CheckedChanged(object sender, EventArgs e)
+        {
+            btn2.Enabled = !btn2.Enabled;
+            btn3.Enabled = !btn3.Enabled;
+            btn4.Enabled = !btn4.Enabled;
+            btn5.Enabled = !btn5.Enabled;
+            btn6.Enabled = !btn6.Enabled;
+            btn7.Enabled = !btn7.Enabled;
+            btn8.Enabled = !btn8.Enabled;
+            btn9.Enabled = !btn9.Enabled;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Log.Clear(tbOut);
+        }
+
+        private void btnNumeric_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (rbBinary.Checked)
+                    Log.addDigit(tbOut, ((Button)sender).Text, true);
+                else
+                    Log.addDigit(tbOut, ((Button)sender).Text, false);
+            }
+            catch
+            {
+                SystemSounds.Beep.Play();
+            }
+            
+        }
+
+        private void btnPoint_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Log.addPoint(tbOut);
+            }
+            catch
+            {
+                SystemSounds.Beep.Play();
+            }
+        }
+
+        private void btnBackspace_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Log.delDigit(tbOut);
+            }
+            catch
+            {
+                SystemSounds.Beep.Play();
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Log.SaveResult(tbOut, rbDecimal.Checked);
+                Log.newNumber(tbOut);
+            }
+            catch
+            {
+                SystemSounds.Beep.Play();
+            }
+
+        }
+
+        private void btnLocalClear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Log.delActive(tbOut);
+            }
+            catch
+            {
+                SystemSounds.Beep.Play();
+            }
+        }
+
+        private void btnNegPos_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Log.NegPosChange(tbOut);
+            }
+            catch
+            {
+                SystemSounds.Beep.Play();
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox fAbout = new AboutBox();
+            fAbout.Show();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter writer = new StreamWriter(saveFile.FileName);
+                writer.Write(tbOut.Text);
+                writer.Close();
+            }
+        }
+
+        private void fileToolStripMenuItem_DropDownOpening(object sender, EventArgs e)
+        {
+            saveToolStripMenuItem.Enabled = tbOut.Text.Length != 0;
+        }
+    }
+}
